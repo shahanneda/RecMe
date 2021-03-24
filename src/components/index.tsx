e2d70f2b2ecf8ab178aa08cf81daba1b6d918eae
shahanneda <shahan.neda@gmail.com>
2021-03-23 23:05:25 -0700

    restarted project tech stack

--- src/components/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { App } from "./App";
-ReactDOM.render(
-  <App url="192.168.1.99:4564" />,
-  document.getElementById("output")
-);
