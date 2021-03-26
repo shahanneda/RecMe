2e79e258c22a3dbf73d419878b8ef97216bb3994
shahanneda <shahan.neda@gmail.com>
2021-03-25 21:59:08 -0700

    added login page

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -1,50 +1,57 @@
 
 import React from 'react'
-import { StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { GestureResponderEvent, StatusBar, StyleSheet, View } from 'react-native'
 import { RColors } from './RColors'
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import { Button, Input, Text } from 'react-native-elements';
+import { Overlay } from 'react-native-elements/dist/overlay/Overlay';
+
 
 interface LoginProps {
-
+    onClose: () => void,
+    setLoginInfo: (info: LoginInfo) => void,
 }
-
-
 const Login = (props: LoginProps) => {
+
+     const loginButtonPress = (e:GestureResponderEvent) => {
+         props.setLoginInfo({
+             loggedIn: true,
+             displayName: "shahaenda",
+             username:"shahanneda",
+             email: "e@example.com",
+             sessionID:"123temp123",
+         });
+
+     } 
+
     return (
-        <View style={styles.container}>
-            <Text>
-                <View style={styles.errorView}>
-                    <Text style={styles.errorText}>
-                        Invalid Password/Username!
-                    </Text>
-                </View>
-                <View style={styles.inputView}>
-                    <TextInput
-                        style={styles.TextInput}
-                        placeholder="username"
-                        placeholderTextColor={RColors.text}
-                        onChangeText={(username) => { }}
-                    />
-                </View>
-
-                <View style={styles.inputView}>
-                    <TextInput
-                        style={styles.TextInput}
-                        placeholder="Password"
-                        placeholderTextColor={RColors.text}
-                        secureTextEntry={true}
-                        onChangeText={(password) => { }}
-                    />
-                </View>
-
-                {/* <TouchableOpacity>
-                    <Text style={styles.forgot_button}>Forgot Password?</Text>
-                </TouchableOpacity> */}
-
-                <TouchableOpacity style={styles.loginBtn}>
-                    <Text style={styles.loginText}>LOGIN</Text>
-                </TouchableOpacity>
+        <Overlay isVisible={true} onBackdropPress={props.onClose}>
+            <View style={styles.container}>
+                <Text h1 h1Style={{
+                    marginBottom: 20,
+                    borderBottomColor: "black",
+                    borderBottomWidth: 1,
+                }}>
+                    Login
             </Text>
-        </View >
+                <Input
+                    placeholder='username'
+                />
+                <Input
+                    placeholder='password'
+                    textContentType='password'
+                    secureTextEntry={true}
+                />
+                <Button title='Login'
+                    buttonStyle={{
+                        width: "100%",
+                        paddingHorizontal: 100,
+                    }}
+                    onPress={loginButtonPress}
+                />
+            </View >
+        </Overlay>
     )
 }
 
@@ -56,66 +63,16 @@ const styles = StyleSheet.create({
         backgroundColor: RColors.background,
         alignItems: 'center',
         justifyContent: 'center',
+        paddingHorizontal: 500,
+        paddingVertical: 100,
     },
-    image: {
-        marginBottom: 40,
-    },
-
-    inputView: {
-        backgroundColor: RColors.light,
-        borderRadius: 30,
-        width: "100%",
-        height: 45,
-        marginBottom: 20,
-        textAlign: "center",
-        alignItems: "center",
-        color: RColors.text,
-    },
-
-    TextInput: {
-        height: 50,
-        flex: 1,
-        padding: 10,
-        textAlign: "center",
-        color: RColors.text,
-    },
-
-    forgot_button: {
-        height: 30,
-        marginBottom: 30,
-    },
-
-    loginBtn: {
-        width: "100%",
-        borderRadius: 25,
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: 20,
-        backgroundColor: RColors.accent,
-    },
-    loginText: {
-        color: RColors.text,
-    },
-    errorText: {
-        textAlign: "center",
-        fontSize: 50,
-        color: "red",
-    },
-    errorView:{
-        margin: "15",
-        padding: "200",
-        backgroundColor: RColors.background2,
-        borderStyle: 'solid',
-        borderWidth:1,
-        borderRadius:2,
-        borderColor: "gray",
-
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.8,
-        shadowRadius: 2,  
-        elevation: 5
-    }
-
 })
+
+interface LoginInfo {
+    loggedIn: boolean,
+    sessionID?: string,
+    username?: string,
+    displayName?: string,
+    email?: string,
+}
+export type { LoginInfo };
